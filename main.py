0eb07ed7552a26ceadecaa78c51aa0b8e5aceedf
Ines <ines.bouakkaoui@gmail.com>
2022-10-25 22:48:58 +0200

    Creation of first game window

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 # Space Invader in Pyhton (good luck à moi-même)
-#je teste les commandes de commentaires
+#test of comments
+import pygame
+
+#Initialize pygame
+pygame.init()
+
+#Create game screen : ! 2 brackets because 2 variables (tuple) !
+screen = pygame.display.set_mode((800,600))
+
+#Game Loop + Allowing to close screen window
+running = True
+
+while running:
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT :
+            running = False
