5e5d1c6af3bc57d54272b0f302cfe73bbf51489a
Ines <ines.bouakkaoui@gmail.com>
2022-10-31 17:25:32 +0100

    Title, logo and background

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Space Invader in Pyhton (good luck à moi-même)
+# Space Invader in Pyhton (good luck to myself)
 #test of comments
 import pygame
 
@@ -8,6 +8,11 @@ pygame.init()
 #Create game screen : ! 2 brackets because 2 variables (tuple) !
 screen = pygame.display.set_mode((800,600))
 
+#Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('alien.png')
+pygame.display.set_icon(icon)
+
 #Game Loop + Allowing to close screen window
 running = True
 
@@ -15,3 +20,8 @@ while running:
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
             running = False
+
+    #Screen color : RGB - Red, Green, Blue,
+    screen.fill((0, 25, 51))
+    pygame.display.update()
+
